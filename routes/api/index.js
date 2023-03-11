f9e106192f545438905adac41554c07f90ab6abb
Tom Ko <yungsu2391@gmail.com>
2023-03-12 06:51:03 +0900

    s3 multer 적용

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express();
 const book = require('./book');
-const user = require('./user')
+const user = require('./user');
+const utils = require('./utils');
 
 router.use("/book", book);
 router.use("/user", user);
+router.use("/utils", utils);
 
 module.exports = router;
--- routes/api/utils/util.controller.js
+++ routes/api/utils/util.controller.js
@@ -1,9 +1,23 @@
 const aws = require('aws-sdk');
+const fs = require('fs')
 const s3 = new aws.s3({
     accessKeyId : process.env.AWS_ACCESS_KEY,
     secretAccessKey : process.env.AWS_ACCESS_SECRET,
 })
 
 exports.profileImageUploader = async (req, res, next) => {
+    const params = {
+        Bucket : 'elasticbeanstalk-ap-northeast-2-053255126826',
+        Key : 'profileImage',
+        Body : fs.readFileSync(req.body.profile_img),
+        ContentType: "image/*"
+    }
 
+    s3.upload(params, function(err, data){
+        if(err){
+            throw err;
+        }
+        console.log('File uploaded successfully');
+        return res.json(data);
+    })
 }
